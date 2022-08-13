590d222e239bab96e3f569efc8dff3171253fbac
tsuruo <tsuru.dev@gmail.com>
2022-08-13 23:54:13 +0900

    Add signout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,40 +11,83 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(signedInProvider).value;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Firebase MFA'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
-      ),
+      body: user == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : _Body(user: user),
     );
   }
 }
 
-// class _ListTile extends StatelessWidget {
-//   const _ListTile({
-//     super.key,
-//     required this.title,
-//     required this.value,
-//   });
+class _Body extends StatelessWidget {
+  const _Body({required this.user});
 
-//   final String title;
-//   final String? value;
+  final User user;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final theme = Theme.of(context);
-//     final colorScheme = theme.colorScheme;
-//     return ListTile(
-//       visualDensity: VisualDensity.compact,
-//       title: Text(title),
-//       trailing: Text(
-//         value ?? '---',
-//         style: theme.textTheme.bodyText2!.copyWith(
-//           color: value == null ? theme.disabledColor : colorScheme.primary,
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    final values = <String, String?>{
+      'uid': user.uid,
+      'displayName': user.displayName,
+      'email': user.email,
+      'emailVerified': user.emailVerified.toString(),
+      'lastSignInTime': user.metadata.lastSignInTime.toString(),
+      'providerId': user.providerData.first.providerId,
+    };
+    return Column(
+      children: [
+        ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: values.entries.toList().length,
+          itemBuilder: (context, index) {
+            final value = values.entries.toList()[index];
+            return _ListTile(
+              title: value.key,
+              value: value.value,
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+          },
+          child: const Text('Sign out'),
+        )
+      ],
+    );
+  }
+}
+
+class _ListTile extends StatelessWidget {
+  const _ListTile({
+    required this.title,
+    required this.value,
+  });
+
+  final String title;
+  final String? value;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    return ListTile(
+      visualDensity: VisualDensity.compact,
+      title: Text(title),
+      trailing: Text(
+        value ?? '---',
+        style: theme.textTheme.bodyText2!.copyWith(
+          color: value == null ? theme.disabledColor : colorScheme.primary,
+        ),
+      ),
+    );
+  }
+}
