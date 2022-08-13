1503d9b5addb32865914bf17aa39ab749f89979a
tsuruo <tsuru.dev@gmail.com>
2022-08-13 22:09:37 +0900

    firebase plugin setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_mfa/firebase_options.dart';
 import 'package:flutter_firebase_mfa/home.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -7,14 +9,22 @@ import 'package:simple_logger/simple_logger.dart';
 
 import 'logger.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   logger.setLevel(
-    kReleaseMode ? Level.SEVERE : Level.FINEST,
+    Level.FINEST,
     includeCallerInfo: kDebugMode,
   );
+
   // Webブラウザ表示時のURLから`#`を取り除く
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
+  await Future.wait([
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ),
+  ]);
+
   runApp(
     const ProviderScope(
       child: App(),
