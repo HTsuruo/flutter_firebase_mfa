8c3849f731cd44f0c08086109a2584fb373af02d
tsuruo <tsuru.dev@gmail.com>
2022-08-13 21:53:36 +0900

    initial setup

---
+++ lib/router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_firebase_mfa/home.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      name: 'home',
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [],
+    ),
+  ],
+);
